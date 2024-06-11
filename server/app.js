d295fbd3ef9f855cc4fe26839127f06fdd56790b
Jzerior <jakubjezior@gmail.com>
2024-06-11 19:24:18 +0200

    Initial server functions

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-
+const database = require("./database/mongoose.js")
 var app = express();
 
 app.use(logger('dev'));
